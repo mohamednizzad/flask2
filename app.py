b22e8afa2d97262df1c817d8c5d9ded6b0dc6da8
Mohamed Nizzad <46175480+mohamednizzad@users.noreply.github.com>
2022-09-27 12:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-	app.run(debug=True)
-
 @app.route('/')
 def home():
 	return render_template('register.html')
