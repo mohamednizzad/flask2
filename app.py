0af05efdd7a0ed779804ea1ed7c620e79b03d971
Mohamed Nizzad <46175480+mohamednizzad@users.noreply.github.com>
2022-09-28 11:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('register.html')
+	#return render_template('register.html')
+	return "Hello World"
 
+"""
 @app.route('/register',methods=["POST"])
 def register():
 	if request.method == 'POST':
 		n = request.form.get('fname')
 		return render_template('register.html',fname=n)
+"""
