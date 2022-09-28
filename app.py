0fafc0a0dfb0146529526fb94c85171ec877e295
Mohamed Nizzad <46175480+mohamednizzad@users.noreply.github.com>
2022-09-28 12:34:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return "Hello World"
+if __name__ == "__main__":
+	app.run(debug=True)
 
+@app.route('/')
+@app.route('/register')
+def home():
+	return render_template('register.html')
+
+@app.route('/register',methods=['POST',"GET"])
+def register():
+	if request.method == 'POST':
+		n = request.form.get('fname')
+		return render_template('register.html',fname=n)
